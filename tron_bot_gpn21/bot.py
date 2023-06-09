ffec01c963c4ae1647fc9c996bd6e1cd585de1d3
Christian Hagenest <mail@hagenest.dev>
2023-06-09 18:32:27 +0200

    asyncio

--- tron_bot_gpn21/bot.py
+++ tron_bot_gpn21/bot.py
@@ -1,74 +1,54 @@
-import socket
-import random
+import asyncio
 
-url = "http://gpn-tron.duckdns.org"
-default_ip = "94.45.236.142"
-port = 4000
-default_name = "hagi-tron"
-version = "0.1"
-password = "oieshgfgh432trewoigfh4w98hoihjkj2"
-moves = {
-            "up": (0, -1),
-            "down": (0, 1),
-            "left": (-1, 0),
-            "right": (1, 0)
-        }
+# Dictionary to store wall positions for each player
+walls = {}
 
-class Bot:
-    def __init__(self, name=default_name, version=version):
-        self.name = name
-        self.version = version
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.socket.connect((default_ip, port))
-        self.socket.sendall(bytes(f"join|{self.name}-{self.version}|{password}\n", "utf-8"))
-        # self.socket.sendall(bytes(self.version + "\n", "utf-8"))
-        # self.socket.sendall(bytes("join\n", "utf-8"))
-        self.socket.settimeout(0.5)
-        self.used_pos = []
-        self.current_pos = (0, 0)
-        self.id = 0
+# Store the map size
+map_width = 0
+map_height = 0
 
-    def listen(self):
-        try:
-            return self.socket.recv(1024).decode("utf-8")
-        except socket.timeout:
-            return "Timeout"
-        except socket.error:
-            return "Error"
+async def parse_packet(packet):
+    global walls, map_width, map_height
+    parts = packet.split('|')
+    packet_type = parts[0]
+    
+    if packet_type == 'motd':
+        print(f"Message of the day: {parts[1]}")
+    elif packet_type == 'error':
+        print(f"Error: {parts[1]}")
+    elif packet_type == 'pos':
+        player_id = int(parts[1])
+        x = int(parts[2])
+        y = int(parts[3])
+        if player_id not in walls:
+            walls[player_id] = []
+        walls[player_id].append((x, y))
+    elif packet_type == 'die':
+        dead_player_ids = [int(pid) for pid in parts[1:]]
+        for dead_player_id in dead_player_ids:
+            if dead_player_id in walls:
+                del walls[dead_player_id]
+    elif packet_type == 'game':
+        map_width = int(parts[1])
+        map_height = int(parts[2])
+    # Add other packet type handling here
 
-    def play(self):
-        for res in self.read_line():
-            if res[0] == "game":
-                self.id = int(res[3])
-                self.used_pos = []
-            if res[0] == "pos":
-                if int(res[1]) == self.id:
-                    self.current_pos = (int(res[2]), int(res[3]))
-                self.used_pos.append((int(res[2]), int(res[3])))
-                print("current_pos:", self.current_pos)
-                print("used:", self.used_pos)
-            if res[0] == "tick":
-                dir = ""
-                for i in range(100): # I don't like this
-                    dir = random.choice(list(moves.keys()))
-                    print("Try dir", dir)
-                    future_pos = (self.current_pos[0] + moves[dir][0], self.current_pos[1] + moves[dir][1])
-                    if future_pos not in self.used_pos:
-                        break
-                self.socket.sendall(bytes(f"move|{dir}\n", "utf-8"))
-                print("Move", dir)
-            if res[0] == "lose":
-                print("lose")
-        
-        
-    def read_line(self):
-        while True:
-            res = self.listen()
-            print(res)
-            for line in res.splitlines():
-                yield line.split("|")
+async def send_move(writer, direction):
+    writer.write(f"move|{direction}\n".encode())
+    await writer.drain()
 
-if __name__ == "__main__":
-    bot = Bot()
-    print("init bot")
-    bot.play()
+async def tron_bot():
+    reader, writer = await asyncio.open_connection('94.45.236.142', 4000)
+
+    # Send join packet
+    writer.write(b"join|hagitron-0.4|mypassword\n")
+    await writer.drain()
+
+    while True:
+        data = await reader.readline()
+        packet = data.decode().strip()
+        await parse_packet(packet)
+        # Add logic for determining the bot's next move
+        await send_move(writer, "up")  # Example move
+
+asyncio.run(tron_bot())
